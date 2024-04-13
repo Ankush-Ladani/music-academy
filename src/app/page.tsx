515477293d26f3e26eb62b6996c1a0f9b1720212
Ankush Ladani <astarladani@gmail.com>
2024-04-13 16:44:20 +0530

    Hero Section and Navbar Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import HeroSection from "@/components/heroSection";
+
 export default function Home() {
-  return <h1 className="text-3xl text-center">Home Page</h1>;
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+    </main>
+  );
 }
